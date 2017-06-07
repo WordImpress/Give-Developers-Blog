58431cd265fec7a4915d554fa717f8c83143d1e7
Devin Walker <sdtucson@gmail.com>
2017-06-06 17:21:49 -0700

    Removed duplicate style enqueue Added Github style code highlighting from Kevin

--- wp-content/themes/give-developers/functions.php
+++ wp-content/themes/give-developers/functions.php
@@ -5,7 +5,6 @@
   */
  function give_enqueue_child_theme_styles() {
      wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
  }
  add_action( 'wp_enqueue_scripts', 'give_enqueue_child_theme_styles');
 
