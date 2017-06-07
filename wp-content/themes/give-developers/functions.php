e7cf79d11f1a3f0afa68f035ca3c43e6066752d5
Devin Walker <sdtucson@gmail.com>
2017-06-06 17:47:19 -0700

    Touched up code styles

--- wp-content/themes/give-developers/functions.php
+++ wp-content/themes/give-developers/functions.php
@@ -1,18 +1,19 @@
 <?php
 
- /**
-  * Load Give's Child Theme styles.
-  */
- function give_enqueue_child_theme_styles() {
-     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
- }
- add_action( 'wp_enqueue_scripts', 'give_enqueue_child_theme_styles');
+/**
+ * Load Give's Child Theme styles.
+ */
+function give_enqueue_child_theme_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'give_enqueue_child_theme_styles' );
 
 /**
  * Remove post format support.
  */
 function give_remove_post_format_support() {
-     remove_theme_support( 'post-formats' );
+	remove_theme_support( 'post-formats' );
 }
 
- add_action( 'after_setup_theme', 'give_remove_post_format_support', 11 );
+add_action( 'after_setup_theme', 'give_remove_post_format_support', 11 );
